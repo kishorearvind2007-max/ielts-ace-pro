ff97476f62d78843f9f0fe095142f31a3071f9cc
kishore <arvindkishore931@gmail.com>
2026-03-19 08:43:17 +0000

    Preceding changes

--- src/components/ielts/SpeakingModule.tsx
+++ src/components/ielts/SpeakingModule.tsx
@@ -3,6 +3,7 @@ import { useTest } from './TestProvider';
 import { TopBar } from './TopBar';
 import { speakingContent } from '@/data/ielts-content';
 import { roundIELTS } from '@/lib/scoring';
+import { useAntiCheat } from '@/hooks/use-anti-cheat';
 import { Button } from '@/components/ui/button';
 import { Mic, MicOff, Send, Loader2, ChevronRight, Clock } from 'lucide-react';
 
